6c77e68d1ef8e2d01293fef125b61aa21866ad94
shushanKar7 <shushan.kar97@gmail.com>
2019-11-02 00:37:59 +0400

    Add new tableraw

--- Dynamic Table/draft.js
+++ Dynamic Table/draft.js
@@ -1,27 +1,40 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import { Titles } from "./Titles";
-import {Fragment} from 'react'
+import {Fragment} from 'react';
+import {Input} from "./Input"
 
 
 class App extends Component {
   state = {
     data: [],
-    titles: ["Full Name", "Email", "Company", "Position", "Country", "Edit"],
+    titles: ["Full Name", "Email", "Company", "Position", "Country", "Edit/Delete"],
     inputValues: ["fullName", "email", "Company", "position", "country"],
     //hide/show elements onClick
     showInputValues: false,
-    closeInputValues: false,
+    addNewData:false,
     fullName: "",
     email: "",
     company: "",
     position: "",
-    country: ""
+    country: "",
+    
   };
+ 
   //close input form
+  closeAddForm = () => {
+    this.setState({
+      addNewData: false
+    })
+  }
   closeForm = () => {
     this.setState({
-      closeInputValues: !this.closeInputValues
+      showInputValues:false
+    })
+  }
+  addingNewData = () => {
+    this.setState({
+      addNewData: !this.addNewData
     })
   }
   //get input values for change
@@ -54,14 +67,14 @@ class App extends Component {
         email: this.email.value,
         company: this.company.value,
         position: this.position.value,
-        country: this.country.value
+        country: this.country.value,
       }),
       headers: {
         "Content-type": "application/json; charset=UTF-8"
       }
     })
       .then(res => res.json())
-      .then(json => console.log(json));
+      .then(res => console.log(res))
   };
 
   addData = () => {
@@ -72,7 +85,8 @@ class App extends Component {
         email: "white.walker@gmail.com",
         company: "GOT",
         position: "Night's King",
-        country: "Winterfell"
+        country: "Winterfell",
+        id: ""
       }),
       headers: {
         "Content-type": "application/json; charset=UTF-8"
@@ -91,8 +105,7 @@ class App extends Component {
 
   render() {
     return (
-
-      <div className="main">
+            <div className="main">
         <h1>FIXED TABLE HEADER</h1>
         <div class="tbl-header">
           <table cellpadding="0" cellspacing="0" border="0">
@@ -120,7 +133,10 @@ class App extends Component {
                       <td>{val.position}</td>
                       <td>{val.country}</td>
                       <td>
-                        <i class="fa fa-edit" onClick={() => this.showTD()}></i>
+                        <div className="i-wrapper">
+                        <i class="material-icons" style={{ fontSize:"16px" }} onClick={() => this.showTD()}>&#xe254;</i>
+                        <i class="fa fa-remove" style={{ fontSize:"16px" }} onClick={() => this.deleteRow()}></i>
+                        </div>
                       </td>
                     </tr>
                   );
@@ -128,7 +144,6 @@ class App extends Component {
               : null}
           </table>
         </div>
-        
         {/* Show input form while clicking on edit icon */}
         {this.state.showInputValues  ? (
           <div className="main2">
@@ -163,7 +178,39 @@ class App extends Component {
             </div> 
             </div>
         ) : null}
-    
+      {this.state.addNewData?
+      <div className="main2">
+      <input
+        placeholder="Full Name"
+        ref={el => (this.fullName = el)}
+        name="fullName" className="allInput"
+      ></input><br/>
+      <input
+        placeholder="Email"
+        ref={el => (this.email = el)}
+        name="email" className="allInputs"
+      ></input><br/>
+      <input
+        placeholder="Company"
+        ref={el => (this.company = el)}
+        name="company" className="allInputs"
+      ></input><br/>
+      <input
+        placeholder="Position"
+        ref={el => (this.position = el)}
+        name="position" className="allInputs"
+      ></input><br/>
+      <input
+        placeholder="Cuntry"
+        ref={el => (this.country = el)}
+        name="country" className="allInputs"
+      ></input><br/>
+      <div className="btnWrapper">
+     <button onClick={this.changeData} className="btn2">Add</button> 
+     <button onClick={this.closeAddForm} className="btn2">Close</button>
+      </div> 
+      </div>
+  : null }
         <div class="made-with-love">
           Made with
           <i>♥</i> by
@@ -175,6 +222,7 @@ class App extends Component {
         <button onClick={this.getData} className="btn">
           <strong>Show Data</strong>{" "}
         </button>
+        <button onClick={this.addingNewData} className="btn1"><strong>Add Data</strong></button>
       </div>
     );
   }
