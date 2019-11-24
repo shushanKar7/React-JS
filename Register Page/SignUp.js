8af1f826286fd6c31429d3c6d5cb756a8c09355f
shushanKar7 <shushan.kar97@gmail.com>
2019-11-25 01:31:59 +0400

    Added validation

--- Register Page/SignUp.js
+++ Register Page/SignUp.js
@@ -1,44 +1,147 @@
 import React from "react";
+import { Component } from "react";
 import { Fragment } from "react";
 
-const SignUp = () => {
-    return (
-        <Fragment>
-           <div className="wrapper">
-        <div className="box1">
-            <div className="content">
-                <div className="logo"></div>
-            </div>
-        </div>
-        <div className="box2">
-            <div className="content">
-                <h2 className="title">Sign Up</h2>
-                <form>
-                    <div>
-                        <input type="text" required name="name" id="name" placeholder="Enter Your Name" />
-                    </div>
-                    <div>
-                        <input type="text" required name="surname" id="surname" placeholder="Enter Your Surname" />
-                    </div>
-                    <div>
-                        <input type="email" required name="email" id="email" placeholder="Enter Your Email" />
-                    </div>
-                    <div>
-                        <input type="password" required name="password" id="password" placeholder="Enter Your Password" />
-                    </div>
-                    <div>
-                        <input type="password" required name="password" id="password" placeholder="Confirm your Password" />
-                    </div>
-                    <div>
-                    <button className="submit">Sign Up</button>
-                    </div>
-                </form>
-                <p>Already have an account? <a class="changer" href="#">Sign in</a> </p>
-            </div>
-        </div>
-    </div>
-        </Fragment>
-    );
+const validEmailRegex = RegExp(
+  /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+);
+const validateForm = errors => {
+  let valid = true;
+  Object.values(errors).forEach(val => val.length > 0 && (valid = false));
+  return valid;
 };
 
-export default SignUp
+const countErrors = errors => {
+  let count = 0;
+  Object.values(errors).forEach(val => val.length > 0 && (count = count + 1));
+  return count;
+};
+
+class SignUp extends Component {
+  state = {
+    formValid: false,
+    errorCount: null,
+    errors: {
+      name: "",
+      surname: "",
+      email: "",
+      password: ""
+    }
+  };
+
+  handleChange = event => {
+    event.preventDefault();
+    const { name, value } = event.target;
+    let errors = this.state.errors;
+
+    switch (name) {
+      case "name":
+        errors.name = value.length < 5 ? "Name must be 5 characters long!" : "";
+        break;
+      case "email":
+        errors.email = validEmailRegex.test(value) ? "" : "Email is not valid!";
+        break;
+
+      case "password":
+        errors.password =
+          value.length < 8 ? "Password must be 8 characters long!" : "";
+        break;
+      default:
+        break;
+    }
+
+    this.setState({ errors, [name]: value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.setState({ formValid: validateForm(this.state.errors) });
+    this.setState({ errorCount: countErrors(this.state.errors) });
+  };
+
+  render() {
+    const { errors, formValid } = this.state;
+    return (
+      <Fragment>
+        <div className="wrapper">
+          <div className="box1">
+            <div className="content">
+              <div className="logo"></div>
+            </div>
+          </div>
+         <div className="box2">
+         <div className="content">
+         <h2 className="title">Sign Up</h2>
+            <form  onSubmit={this.handleSubmit} noValidate>
+            <div>
+                <input
+                  type="text"
+                  required
+                  name="name"
+                  id="name"
+                  placeholder="Enter Your Name"
+                  onChange={this.handleChange} noValidate
+                />
+                 {errors.name.length > 0 && 
+                  <span className='error'>{errors.name}</span>}
+              </div>
+              <div>
+                <input
+                  type="text"
+                  required
+                  name="surname"
+                  id="surname"
+                  placeholder="Enter Your Surname"
+                />
+              </div>
+              <div>
+                <input
+                  type="email"
+                  required
+                  name="email"
+                  id="email"
+                  placeholder="Enter Your Email"
+                  onChange={this.handleChange} noValidate
+                />
+                {errors.email.length > 0 && 
+                <span className='error'>{errors.email}</span>}
+              </div>
+              <div>
+                <input
+                  type="password"
+                  required
+                  name="password"
+                  id="password"
+                  placeholder="Enter Password"
+                  onChange={this.handleChange} noValidate
+                />
+                {errors.password.length > 0 && 
+                 <span className='error'>{errors.password}</span>}
+              </div>
+              <div>
+                <input
+                  type="password"
+                  required
+                  name="password"
+                  id="password"
+                  placeholder="Confirm your Password"
+                />
+              </div>
+              <div>
+                <button className="submit">Sign Up</button>
+              </div>
+            </form>
+            <p>
+              Already have an account?{" "}
+              <a class="changer" href="#">
+                Sign in
+              </a>{" "}
+            </p>
+             </div>   
+         </div>
+        </div>
+      </Fragment>
+    );
+  }
+}
+export default SignUp;
--- Register Page/SingIn.js
+++ Register Page/SingIn.js
@@ -1,10 +1,70 @@
-import React from "react";
-import { Fragment } from "react";
+import React from 'react'
+import { Component } from 'react'
+import { Fragment } from 'react'
 
-const SignIn = () => {
+const validEmailRegex = RegExp(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i);
+const validateForm = (errors) => {
+  let valid = true;
+  Object.values(errors).forEach(
+    (val) => val.length > 0 && (valid = false)
+  );
+  return valid;
+}
+
+const countErrors = (errors) => {
+  let count = 0;
+  Object.values(errors).forEach(
+    (val) => val.length > 0 && (count = count+1)
+  );
+  return count;
+}
+
+class SignIn extends Component {
+    state = {
+      formValid: false,
+      errorCount: null,
+      errors: {
+        username: '',
+        password: '',
+      }
+    }
+
+  handleChange = (event) => {
+    event.preventDefault();
+    const { name, value } = event.target;
+    let errors = this.state.errors;
+
+    switch (name) {
+      case 'username': 
+        errors.username = 
+          value.length < 5
+            ? 'Username must be 5 characters long!'
+            : '';
+        break;
+      case 'password': 
+        errors.password = 
+          value.length < 8
+            ? 'Password must be 8 characters long!'
+            : '';
+        break;
+      default:
+        break;
+    }
+
+    this.setState({errors, [name]: value});
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({formValid: validateForm(this.state.errors)});
+    this.setState({errorCount: countErrors(this.state.errors)});
+  }
+
+  render() {
+    const {errors, formValid} = this.state;
     return (
-        <Fragment>
-            <div className="wrapper">
+      <Fragment>
+      <div className="wrapper">
         <div className="box1">
           <div className="content">
             <div className="logo"></div>
@@ -13,7 +73,7 @@ const SignIn = () => {
         <div className="box2">
           <div className="contentSignIn">
             <h2 className="title">Sign In</h2>
-            <form>
+            <form onSubmit={this.handleSubmit} noValidate>
               <div>
                 <input
                   type="text"
@@ -21,7 +81,10 @@ const SignIn = () => {
                   name="username"
                   id="username"
                   placeholder="Enter Your Name"
+                  onChange={this.handleChange} noValidate
                 />
+                 {errors.username.length > 0 && 
+                  <span className='error'>{errors.username}</span>}
               </div>
               <div>
                 <input
@@ -30,7 +93,10 @@ const SignIn = () => {
                   name="password"
                   id="password"
                   placeholder="Enter Password"
+                  onChange={this.handleChange} noValidate
                 />
+                {errors.password.length > 0 && 
+                 <span className='error'>{errors.password}</span>}
               </div>
               <div>
                 <button className="submit">Sign In</button>
@@ -45,8 +111,8 @@ const SignIn = () => {
           </div>
         </div>
       </div>
-        </Fragment>
-    );
-};
-
-export default SignIn;
+    </Fragment>
+    )
+  }
+}
+export default SignIn
