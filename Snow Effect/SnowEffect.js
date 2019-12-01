036533a542f9fe07f8e7a084163490299ace7462
shushanKar7 <shushan.kar97@gmail.com>
2019-12-02 02:31:39 +0400

    Snow Effect with pure CSS

---
+++ Snow Effect/SnowEffect.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Fragment } from "react";
+
+const SnowEffect = () => {
+  return (
+    <Fragment>
+      <div className="snowContainer">
+        <div className="snowImageBg"></div>
+        <div class="_150 snowfield">
+          <p>.</p>
+          <p>.</p>
+          <p>.</p>
+          <p>.</p>
+          <p>.</p>
+        </div>
+        <div class="_130 snowfield">
+          <p>.</p>
+          <p>.</p>
+          <p>.</p>
+          <p>.</p>
+          <p>.</p>
+        </div>
+        <div class="_90 snowfield">
+          <p>.</p>
+          <p>.</p>
+          <p>.</p>
+          <p>.</p>
+          <p>.</p>
+        </div>
+        <div class="_160 snowfield">
+          <p>.</p>
+          <p>.</p>
+          <p>.</p>
+          <p>.</p>
+          <p>.</p>
+        </div>
+        <div class="_100 snowfield">
+          <p>.</p>
+          <p>.</p>
+          <p>.</p>
+          <p>.</p>
+          <p>.</p>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default SnowEffect;
